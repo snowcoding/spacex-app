c20772e7332f19842c0871609e39558046bf7d07
snowcoding <vimalshah77@gmail.com>
2019-07-10 12:58:23 -0700

    [feat]: Adds Sentry for error detection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Apollo from './components/Apollo/Apollo'
+//Sentry
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({
+  dsn: 'https://855268d3a8084bd88c551f3cc49ec6b4@sentry.io/1501878',
+  environment: 'development',
+})
 
 ReactDOM.render(
   <Apollo>
