d937abb991ab4a689602567bb29c950f2551d1b0
snowcoding <vimalshah77@gmail.com>
2019-07-12 03:18:24 -0700

    [chore]: Adds snapshot tests to select components

--- src/components/ChartYaxisLabel/ChartYAxisLabel.test.js
+++ src/components/ChartYaxisLabel/ChartYAxisLabel.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ChartYaxisLabel from './ChartYaxisLabel'
+import renderer from 'react-test-renderer'
+
+it('snapshot test Y axis Label of Chart', () => {
+  const yAxisLabel = { title: 'title', popover: 'popever', link: 'link' }
+  const tree = renderer
+    .create(<ChartYaxisLabel yAxisLabel={yAxisLabel} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  const yAxisLabel = {title:'title', popover:'popever', link:'link'}
-  ReactDOM.render(
-      <ChartYaxisLabel yAxisLabel={yAxisLabel}/>,
-    div
-  )
+  const yAxisLabel = { title: 'title', popover: 'popever', link: 'link' }
+  ReactDOM.render(<ChartYaxisLabel yAxisLabel={yAxisLabel} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/components/FilterDateRange/FilterDateRange.test.js
+++ src/components/FilterDateRange/FilterDateRange.test.js
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import FilterDateRange from './FilterDateRange'
 import LaunchProvider from '../../contexts/LaunchProvider'
+import renderer from 'react-test-renderer'
+
+it('snapshot test Date Range Filter', () => {
+  const tree = renderer
+    .create(
+      <LaunchProvider>
+        <FilterDateRange />
+      </LaunchProvider>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
